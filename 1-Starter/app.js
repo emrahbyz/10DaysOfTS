57c65be8ba94a3acda294c232196e4ef2bd9bd2f
Emrah <146847947+emrahbyz@users.noreply.github.com>
2024-03-23 01:24:15 +0300

    data type

--- 1-Starter/app.js
+++ 1-Starter/app.js
@@ -1,2 +1,61 @@
-console.log("test 1");
-console.log("test");
+//                   Data Type --Number-String-Boolean--
+var first = 123;
+// console.log(first);
+var firstName = "emrah";
+var lastName = "beyazıt";
+var fullName = "".concat(firstName, " amazing its work (: ").concat(lastName, " yes true :D ");
+// console.log(fullName);
+var isPresent = true;
+// console.log(isPresent);
+//                        --Array--
+var firstNames = ["emrah", "beyazıt", "3", "4"];
+var arr = [2, "test", true];
+var values = ["test", 2];
+console.log(firstNames.length);
+console.log(arr);
+console.log(values);
+console.log(values[1]);
+// for (let index = 0; index < firstNames.length; index++) {
+//   console.log(firstNames[index]);
+// }
+// firstNames.map((item) => {
+//   return console.log(item);
+// });
+//                        --Tuple--
+var instructor = [1, "emrah"];
+console.log(instructor);
+var user;
+user = [1, "test", true];
+console.log(user);
+var arrTest;
+arrTest = [
+    [1, "emrah"],
+    [2, "beyazıt"],
+];
+console.log(arrTest);
+//                        --Object--
+var person;
+person = {
+    firstName: "emrah",
+    lastName: "beyazıt",
+    age: 25,
+    jobTitle: "Software Developer",
+};
+console.log(person.age);
+//                        --Enum--
+//number enum
+var Media;
+(function (Media) {
+    Media[Media["NewsPaper"] = 3] = "NewsPaper";
+    Media[Media["Magazine"] = 4] = "Magazine";
+    Media[Media["Book"] = 5] = "Book";
+})(Media || (Media = {}));
+console.log(Media.Magazine);
+//String enum
+var PrintMedia;
+(function (PrintMedia) {
+    PrintMedia["NewsPaper"] = "newpaper";
+    PrintMedia["Magazine"] = "newsletter";
+    PrintMedia["Book"] = "book";
+})(PrintMedia || (PrintMedia = {}));
+console.log(PrintMedia.Magazine);
--- 1-Starter/app.ts
+++ 1-Starter/app.ts
@@ -1,2 +1,80 @@
-console.log("test 1");
-console.log("test");
+//                   Data Type --Number-String-Boolean--
+
+let first: number = 123;
+// console.log(first);
+
+let firstName: string = "emrah";
+let lastName: string = "beyazıt";
+
+let fullName: string = `${firstName} amazing its work (: ${lastName} yes true :D `;
+// console.log(fullName);
+
+let isPresent: boolean = true;
+// console.log(isPresent);
+
+//                        --Array--
+let firstNames: string[] = ["emrah", "beyazıt", "3", "4"];
+let arr = [2, "test", true];
+let values: (string | number)[] = ["test", 2];
+console.log(firstNames.length);
+console.log(arr);
+console.log(values);
+console.log(values[1]);
+
+// for (let index = 0; index < firstNames.length; index++) {
+//   console.log(firstNames[index]);
+// }
+// firstNames.map((item) => {
+//   return console.log(item);
+// });
+
+//                        --Tuple--
+
+let instructor: [number, string] = [1, "emrah"];
+console.log(instructor);
+let user: [number, string, boolean];
+user = [1, "test", true];
+console.log(user);
+let arrTest: [number, string][];
+arrTest = [
+  [1, "emrah"],
+  [2, "beyazıt"],
+];
+console.log(arrTest);
+
+//                        --Object--
+
+let person: {
+  firstName: string;
+  lastName: string;
+  age: number;
+  jobTitle: string;
+};
+
+person = {
+  firstName: "emrah",
+  lastName: "beyazıt",
+  age: 25,
+  jobTitle: "Software Developer",
+};
+
+console.log(person.age);
+
+//                        --Enum--
+//number enum
+
+enum Media {
+  NewsPaper = 3,
+  Magazine,
+  Book,
+}
+console.log(Media.Magazine);
+
+//String enum
+
+enum PrintMedia {
+  NewsPaper = "newpaper",
+  Magazine = "newsletter",
+  Book = "book",
+}
+console.log(PrintMedia.Magazine);
