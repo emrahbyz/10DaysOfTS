6d36494da2d0bb39cf59e3c19b97daf413b0db42
Emrah <146847947+emrahbyz@users.noreply.github.com>
2024-04-29 03:16:51 +0300

    typescript new commit

--- vite-project/src/components/TsTest.tsx
+++ vite-project/src/components/TsTest.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 type TestProps = {
   name?: string;
@@ -6,8 +6,19 @@ type TestProps = {
 };
 
 const TsTest: FC<TestProps> = ({ name, age }) => {
+  const [first, setFirst] = useState<number>(22);
+  useEffect(() => {
+    setFirst(first);
+  });
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Button clicked!");
+    console.log("Event details:", event);
+  };
+
   return (
     <div>
+      <button onClick={(event) => handleClick(event)}>Test</button>
+      {first}
       {name}
       {age}
     </div>
